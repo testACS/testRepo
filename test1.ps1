9b115486b49b1aba3db85a977cd26a1f00abf085
testACS <test@agilecloudsolutions.co.uk>
2017-04-09 10:19:30 +0100

    stg

---
+++ test1.ps1
@@ -0,0 +1,19 @@
+[CmdletBinding()]
+Param
+(
+[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+[string]$Name,
+[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+[int64]$EmployeeId,
+[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+[string]$Department,
+[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
+[string]$City
+)
+
+Process
+{
+Write-Output "============================"
+Write-Output "Name: $Name`nDepartment: $Department`nEmployeeId: $EmployeeId`nCity: $City"
+Write-Output "============================"
+}
