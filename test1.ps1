b0e8deca95bf876cba35503bd9378f9463b76bf5
Gowtham Jakka <Gowtham.Jakka@river-island.com>
2017-04-25 14:35:34 +0100

    test commit

--- test1.ps1
+++ test1.ps1
@@ -1,19 +1,5 @@
-[CmdletBinding()]
-Param
-(
-    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [string]$Name,
-    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [int64]$EmployeeId,
-    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [string]$Department,
-    [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName,ValueFromPipeline)]
-    [string]$City
-)
 
-Process
-{
-    Write-Output "============================"
-    Write-Output "Name: $Name`nDepartment: $Department`nEmployeeId: $EmployeeId`nCity: $City"
-    Write-Output "============================"
-}
+
+Get-S3Object -BucketName prod-cloudwatch-log-archive -Key prod-log-groups-prodRIWebsiteLogs-1QEXT9O7CJEO9 -Region eu-west-1 -AccessKey $AKProd -SecretKey $SKProd -Verbose | `
+select BucketName, @{n="File";e={($_.Key)}}, LastModified, @{n="Size";e={[math]::Round(($_.Size / 1MB ), 2)}} | `
+? {$_.LastModified -ge ([DateTime]::Today.AddDays(-2).AddHours(18)) -and ($_.File -ne '/aws-logs-w')} | sort LastModified
